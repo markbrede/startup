c329e4cb61553ac2ec588b8e561798520cfe5ad4
markbrede <markjacobbrede@gmail.com>
2024-12-10 11:54:09 -0700

    Created App Component with the header and foot HTML

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import '../index.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className="body img-container">
+      <header>
+        <h1>Auto Expense Tracker</h1>
+        <nav>
+          <menu>
+            <li><a href="index.html" className="active">Home</a></li>
+            <li><a href="track.html">Track Expenses</a></li>
+            <li><a href="history.html">Expense History</a></li>
+            <li><a href="about.html">About</a></li>
+          </menu>
+        </nav>
+        <hr />
+      </header>
+
+      <footer>
+        <hr />
+        <span className="text-reset">Mark Brede</span>
+        <br />
+        <a href="https://github.com/markbrede/startup/tree/main">GitHub</a>
+      </footer>
+    </div>
+  );
 }
