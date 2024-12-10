0c101d2a19c4fb9118962d6433b5f0fe4acb8c12
markbrede <markjacobbrede@gmail.com>
2024-12-10 12:16:29 -0700

    Created veiw components/router

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 import './app.css';
 import '../index.css';
 
-export default function App() {
+function Layout() {
   return (
     <div className="body img-container">
       <header>
         <h1>Auto Expense Tracker</h1>
         <nav>
           <menu>
-            <li><a href="index.html" className="active">Home</a></li>
-            <li><a href="track.html">Track Expenses</a></li>
-            <li><a href="history.html">Expense History</a></li>
-            <li><a href="about.html">About</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/track">Track Expenses</Link></li>
+            <li><Link to="/history">Expense History</Link></li>
+            <li><Link to="/about">About</Link></li>
           </menu>
         </nav>
         <hr />
       </header>
+
+      <main>
+        <Outlet />
+      </main>
 
       <footer>
         <hr />
@@ -27,3 +32,23 @@ export default function App() {
     </div>
   );
 }
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<>login displayed here</>} />
+          <Route path="track" element={<>track expenses displayed here</>} />
+          <Route path="history" element={<>history displayed here</>} />
+          <Route path="about" element={<>about displayed here</>} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>Error 404: Address unknown.</main>;
+  }
