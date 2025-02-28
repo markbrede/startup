ea3ccd1f9368d594cbe903b76e6139f0cec2a3dc
markbrede <markjacobbrede@gmail.com>
2025-02-28 10:59:15 -0700

    I cant help but feel the original html and css I made was almost entirley pointless. I had to completely restructure my app.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './app.css';
 
+import { Login } from './login/login';
+import { Track } from './track/track';
+import { History } from './history/history';
+import { About } from './about/about';
+
+function NotFound() {
+  return <h1>404: Page not found</h1>;
+}
+
 function App() {
-  return <div className="body">App will display here</div>;
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Auto Expense Tracker</h1>
+        <nav>
+          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/track">Track Expenses</NavLink>
+          <NavLink to="/history">Expense History</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/track' element={<Track />} />
+          <Route path='/history' element={<History />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+
+      <footer>
+        <p>Mark Brede</p>
+        <a href="https://github.com/markbrede/startup/tree/main">GitHub</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/track/track.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Track() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Track displayed here</div>
+    </main>
+  );
+}
