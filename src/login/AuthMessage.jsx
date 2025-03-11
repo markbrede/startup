e9b622251dba0c90ffcc67b097a15fc21b81357b
markbrede <markjacobbrede@gmail.com>
2025-03-11 11:36:33 -0600

    Athentication message for incorrect login

---
+++ src/login/AuthMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AuthMessage() {
+  return (
+    <div>
+      <h2>Authentication Failed</h2>
+      <p>Please check your credentials and try again.</p>
+    </div>
+  );
+}
